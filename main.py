8e606827fa5c990e587435da0d7f094ce1d59d6b
Oscar Torres Garcia <oscar.torres5623@alumnos.udg.mx>
2020-09-30 11:30:26 -0700

    prueba01

--- main.py
+++ main.py
@@ -1,1 +1,55 @@
-print("Hola Mundo")
+#Programa para determinar el signo zodiacal de una persona
+#Inicializando las variables
+dia=mes=0
+#Entrada
+dia=int(input("Dame el dia: "))
+mes=int(input("Dame el mes: "))
+#Proceso
+if dia>=22 and dia<=31 and mes==12:
+      print("Capricornio")
+elif dia<=20 and dia>=1 and mes==1:
+      print("Capricornio")
+if dia>=21 and dia<=31 and mes==1:
+      print("Acuario")
+elif dia<=19 and dia>=1 and mes==2:
+      print("Acuario")
+if dia>=20 and dia<=28 and mes==2:
+      print("Piscis")
+elif dia<=20 and dia>=1 and mes==3:
+      print("Piscis")
+if dia>=21 and dia<=31 and mes==3:
+      print("Aries")
+elif dia<=20 and dia>=1 and mes==4:
+      print("Aries")
+if dia>=21 and dia<=30 and mes==4:
+      print("Tauro")
+elif dia<=21 and dia>=1 and mes==5:
+      print("Tauro")
+if dia>=22 and dia<=31 and mes==5:
+      print("Geminis")
+elif dia<=21 and dia>=1 and mes==6:
+      print("Geminis")
+if dia>=22 and dia<=30 and mes==6:
+      print("Cancer")
+elif dia<=22 and dia>=1 and mes==7:
+      print("Cancer")
+if dia>=23 and dia<=31 and mes==7:
+      print("Leo")
+elif dia<=23 and dia>=1 and mes==8:
+      print("Leo")
+if dia>=24 and dia<=31 and mes==8:
+      print("Virgo")
+elif dia<=22 and dia>=1 and mes==9:
+      print("Virgo")
+if dia>=23 and dia<=30 and mes==9:
+      print("Libra")
+elif dia<=22 and dia>=1 and mes==10:
+      print("Libra")
+if dia>=23 and dia<=31 and mes==10:
+      print("Escorpio")
+elif dia<=21 and dia>=1 and mes==11:
+      print("Escorpio")
+if dia>=22 and dia<=30 and mes==11:
+      print("Sagitario")
+elif dia<=21 and dia>=1 and mes==12:
+      print("Sagitario")
